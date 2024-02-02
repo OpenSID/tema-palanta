70c612763637322d960611938bad0b1040668901
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2024-02-02 09:16:06 +0800

    Update artikel.tpl.php

--- layouts/artikel.tpl.php
+++ layouts/artikel.tpl.php
@@ -12,12 +12,23 @@
 		<div class="default-row mt-20">
 			<div class="container-custom">
 				<div class="row-custom mlr-min-20">
-					<div class="pageleft">
-					<?php $this->load->view("$folder_themes/partials/artikel"); ?>
-					</div>
-					<div class="pageright">
-					<?php $this->load->view("$folder_themes/partials/sidebar"); ?>
-					</div>
+					<?php if ($single_artikel['tampilan'] == 1): ?>
+						<div class="pageleft">
+							<?php $this->load->view("$folder_themes/partials/artikel"); ?>
+						</div>
+						<div class="pageright">
+							<?php $this->load->view("$folder_themes/partials/sidebar"); ?>
+						</div>
+					<?php elseif ($single_artikel['tampilan'] == 2): ?>
+						<div class="pageleft">
+							<?php $this->load->view("$folder_themes/partials/sidebar"); ?>
+						</div>
+						<div class="pageright">
+							<?php $this->load->view("$folder_themes/partials/artikel"); ?>
+						</div>
+					<?php else : ?>
+						<?php $this->load->view("$folder_themes/partials/artikel"); ?>
+					<?php endif ?>
 				</div>
 			</div>
 		</div>
