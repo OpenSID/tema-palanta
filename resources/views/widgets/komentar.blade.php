66b519e29c18d4dfa88fff33e020d5800f6294b8
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2026-07-16 15:11:27 +0800

    update

--- resources/views/widgets/komentar.blade.php
+++ resources/views/widgets/komentar.blade.php
@@ -6,7 +6,7 @@
 		</div>
 	</div>
 	<div class="widgetbox">
-		<marquee onmouseover="this.stop()" onmouseout="this.start()" scrollamount="3" direction="up" width="100%" height="180px" align="center" behavior=”alternate”>
+		<marquee onmouseover="this.stop()" onmouseout="this.start()" scrollamount="3" direction="up" width="100%" height="180px" align="center" behavior="alternate">
 			@foreach($komen As $data)
 			<div class="comment-row">
 				<div class="comment-icon c-flex">
@@ -15,7 +15,7 @@
 				<div class="comment-title">
 					<h3>{{ $data['owner'] }}</h3>
 					<p>{{ tgl_indo2($data['tgl_upload'])}}</p>
-					<p style="margin-top:5px;">{!! potong_teks($data['komentar'], 50) !!}... <a href="{{ ci_route('artikel/' . buat_slug($data))">selengkapnya</a></p>
+					<p style="margin-top:5px;">{!! potong_teks($data['komentar'], 50) !!}... <a href="{{ ci_route('artikel/' . buat_slug($data)) }}">selengkapnya</a></p>
 				</div>
 			</div>	
 			@endforeach
